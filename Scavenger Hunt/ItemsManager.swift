e52ec4e8b4e865f7c86c0f1fd875a8dea69b6ab2
ahadsheriff <ahadsheriff@gmail.com>
2016-02-18 23:51:42 -0500

    first commit

---
+++ Scavenger Hunt/ItemsManager.swift
@@ -0,0 +1,9 @@
+//
+//  ItemsManager.swift
+//  Scavenger Hunt
+//
+//  Created by Ahad Sheriff on 1/26/16.
+//  Copyright © 2016 Ahad Sheriff. All rights reserved.
+//
+
+import Foundation
---
+++ Scavenger Hunt/ScavengerHuntItem.swift
@@ -0,0 +1,9 @@
+//
+//  ScavengerHuntItem.swift
+//  Scavenger Hunt
+//
+//  Created by Ahad Sheriff on 1/26/16.
+//  Copyright © 2016 Ahad Sheriff. All rights reserved.
+//
+
+import Foundation
